6b2047034fbb59f40d5c5ed60f6d6635c8512475
Daniel Allison <hello@danielallison.dev>
2022-09-05 16:10:00 -0400

    Submit p49

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
 mod p217;
 mod p242;
 mod p1;
+mod p49;
 
 fn main() {
     // p217
     let nums: Vec<i32> = vec![2, -3, 13, 6];
     let answer: bool = p217::contains_duplicate(nums);
-    println!("Contains duplicate?: {}", answer);
+    println!("p217: Contains duplicate?: {}", answer);
 
     // p242
     let string1: String = String::from("Hello, world");
     let string2: String = String::from("dlrow, olleH");
 
     let answer: bool = p242::is_anagram(string1, string2);
-    println!("Is anagram?: {}", answer);
+    println!("p242: Is anagram?: {}", answer);
+
+    // p49
+    let strs = vec!["eat".to_string(), "tea".to_string(), "ate".to_string(), "nat".to_string(), "bat".to_string()];
+    let answer = p49::group_anagrams(strs);
+    println!("p49: Grouped anagrams:{:?}", answer);
 }
---
+++ src/p49/mod.rs
@@ -0,0 +1,46 @@
+// Group Anagrams
+// https://leetcode.com/problems/group-anagrams/
+
+pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
+    let mut map = std::collections::HashMap::new();
+    for v in strs {
+        let mut k = v.chars()
+            .collect::<Vec<char>>();
+        k.sort_unstable();
+        map.entry(k).or_insert_with(|| vec![]).push(v);
+    }
+    let mut result = map.into_iter().map(|(_, v)| v).collect::<Vec<Vec<String>>>();
+    
+    for v in &mut result {
+        v.sort_unstable();
+    }
+    
+    result.sort_unstable();
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::group_anagrams;
+
+    #[test]
+    fn example_1() {
+        let input: Vec<String> = vec![String::from("eat"), String::from("tea"), String::from("tan"), String::from("ate"), String::from("nat"), String::from("bat")];
+        let result: Vec<Vec<String>> = group_anagrams(input);
+        assert_eq!(result, vec![vec!["ate", "eat", "tea"], vec!["bat"], vec!["nat", "tan"]]);
+    }
+
+    #[test]
+    fn example_2() {
+        let input: Vec<String> = vec![String::from("")];
+        let result: Vec<Vec<String>> = group_anagrams(input);
+        assert_eq!(result, vec![vec![""]]);
+    }
+
+    #[test]
+    fn example_3() {
+        let input: Vec<String> = vec![String::from("a")];
+        let result: Vec<Vec<String>> = group_anagrams(input);
+        assert_eq!(result, vec![vec!["a"]]);
+    }
+}

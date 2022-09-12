eb854d7aad98e248d84dbd17736da8eb749985f1
Daniel Allison <hello@danielallison.dev>
2022-09-11 22:39:51 -0400

    Submit p584

---
+++ src/p584/schema.sql
@@ -0,0 +1,7 @@
+Create table If Not Exists Customer (id int, name varchar(25), referee_id int);
+insert into Customer (id, name, referee_id) values (1, 'Will', NULL);
+insert into Customer (id, name, referee_id) values (2, 'Jane', NULL);
+insert into Customer (id, name, referee_id) values (3, 'Alex', 2);
+insert into Customer (id, name, referee_id) values (4, 'Bill', NULL);
+insert into Customer (id, name, referee_id) values (6, 'Mark', 2);
+insert into Customer (id, name, referee_id) values (5, 'Zack', 1);

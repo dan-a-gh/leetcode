f7938d68b1a92418e666dc0e64280a3a35572e59
Daniel Allison <hello@danielallison.dev>
2022-09-11 22:26:16 -0400

    Submit p1757

---
+++ src/p1757/schema.sql
@@ -0,0 +1,6 @@
+Create table If Not Exists Products (product_id int, low_fats ENUM('Y', 'N'), recyclable ENUM('Y','N'));
+insert into Products (product_id, low_fats, recyclable) values ('0', 'Y', 'N');
+insert into Products (product_id, low_fats, recyclable) values ('1', 'Y', 'Y');
+insert into Products (product_id, low_fats, recyclable) values ('2', 'N', 'Y');
+insert into Products (product_id, low_fats, recyclable) values ('3', 'Y', 'Y');
+insert into Products (product_id, low_fats, recyclable) values ('4', 'N', 'N');
